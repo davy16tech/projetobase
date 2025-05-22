b0b5d535cc0a5160595a5bc963c82b7c1c30d7bc
davy16tech <Davyzinhodavyalessandro@gmail.com>
2025-05-22 11:14:50 -0300

    Aqui eu vi let,console, e fiz uma calculadora

--- logicacomjs.js
+++ logicacomjs.js
@@ -1,1 +1,23 @@
-console.log("Oi")
+/*let number1  = 245;
+let number2 = 1200;
+let soma = number1+number2
+console.log("A soma é: "+soma)
+console.log("---------------------")
+let number4 = 5;
+let number5 = 12;
+let soma2 = number4*number5
+console.log("a multiplicação é: "+soma2)
+console.log("---------------------")
+let number6 = 24;
+let number7 = 4;
+let soma3 = number6/number7
+console.log("a divisão é: "+soma3)
+console.log("----------------------")
+let number9 = 232
+let number10 = 32
+let soma4 = number9-number10
+console.log("a subtração é: "+soma4)*/
+var nome = "geraldo"
+let decimo1º = 1518/12
+let mesestrab = decimo1º *8
+console.log( nome+", seu decimo terceiro é: R$"+mesestrab +", Parabéns" )
