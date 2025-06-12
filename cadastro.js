fd3dc835856c4bb9dae1fea11098eb6292e4caf7
davy16tech <Davyzinhodavyalessandro@gmail.com>
2025-06-12 11:32:50 -0300

    events

---
+++ cadastro.js
@@ -0,0 +1,12 @@
+function clicou(){
+    let quandoclicar = document.getElementById("botao")
+    quandoclicar.innerText = "Clicou"
+}
+function saiu(){
+    let quandosair = document.getElementById("botao")
+    quandosair.style.background = "green"
+}
+function entrar(){
+    let quandoentrar = document.getElementById("botao")
+    quandoentrar.style.background = "Red"
+}
---
+++ index.js
@@ -0,0 +1,9 @@
+/*let  buscando = document.getElementsByTagName("p")[0]
+console.log(buscando)
+let buscando = document.getElementsByName("h2dadiv")[0]
+buscando.style.background = "Red"
+let buscando = document.getElementById("atalaia")
+buscando.style.background = "Gray"
+getElementsClassName()
+let modorapido = document.querySelector("#h2doroteiro")
+modorapido.style.background = "Red"*/
